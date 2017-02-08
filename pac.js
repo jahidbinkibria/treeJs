c5bb89e8f68384adeadd669727325db5b3ae8935
Jahid Bin Kibria <jahidbinkibria@gmail.com>
2017-02-09 01:47:23 +0600

    new chanhe

---
+++ pac.js
@@ -0,0 +1,8 @@
+var question = 3;
+var first = prompt(question + ' question left','Name');
+question -= 1;
+var second = prompt(question + ' question left',' Id');
+question -= 1;
+var third = prompt(question + ' question left ',' Batch ');
+
+document.write(' you\'r name is ' + first + ' you\'r id : ' +second +' you\'r batch : ' + third);
