b3dac9bb326da1319385331a93b7a2d5ba76122f
Jahid Bin Kibria <jahidbinkibria@gmail.com>
2017-02-09 03:42:03 +0600

    random number first step

--- pac.js
+++ pac.js
@@ -1,9 +1,6 @@
 // start with git
-var question = 3;
-var first = prompt(question + ' question left','Name');
-question -= 1;
-var second = prompt(question + ' question left',' Id');
-question -= 1;
-var third = prompt(question + ' question left ',' Batch ');
-
-document.write(' you\'r name is ' + first + ' you\'r id : ' +second +' you\'r batch : ' + third);
+var input = prompt("enter a number");
+var topNumber = parseInt(input);
+var randomNumber = Math.floor(Math.random() * topNumber)+1;
+var message = "<p>" + randomNumber + " is number between 1 and " + topNumber + "</p>"
+document.write(message);
