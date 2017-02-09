3419797556db95aab44b395f7e4ee4f52b5e11e1
Jahid Bin Kibria <jahidbinkibria@gmail.com>
2017-02-09 13:28:39 +0600

    guess random number while loop

--- pac.js
+++ pac.js
@@ -1,12 +1,14 @@
-function randomfunction(upper){
-
+var upper = 1000;
+var randomNumber = getrandomnumber(upper);
+var guess;
+var attempts = 0; 
+function getrandomnumber(upper){
 	return Math.floor(Math.random() * upper)+1;
-
 }
 
-var count = 0;
-while(count<100){
-	var ramcount = randomfunction(100);
-	document.write(ramcount + ' ');
-	count++
+while(guess !== randomNumber){
+	guess = getrandomnumber(upper);
+	attempts++
 }
+document.write("<p> the random number is " + randomNumber + "</p>");
+document.write("<p> it took the coumputer " + attempts + " attemps to get it right </p>");
