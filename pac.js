fc1c9a645eb24591b26a3cbef6040c6bf81b9f03
Jahid Bin Kibria <jahidbinkibria@gmail.com>
2017-02-10 04:44:34 +0600

    random RGB color

--- pac.js
+++ pac.js
@@ -1,5 +1,24 @@
 var html = '';
-for(var i=1; i<=10; i++){
-	html += "<div>" + i + "</div>";
+var red;
+var green;
+var blue;
+var rgbColor;
+function rgbfunction(){
+	return Math.floor(Math.random() * 256);
 }
-document.write(html); 
+function rgbrandom(){
+	var color = 'rgb(';
+	color += rgbfunction() + ',';
+	color += rgbfunction() + ',';
+	color += rgbfunction() + ')';
+	return color;
+}
+function print(message){
+	document.write(message);
+}
+for(var i = 1;i<=112;i++){
+	rgbColor = rgbrandom();
+	html += '<div style="background-color:' + rgbColor + '"></div>';
+}
+
+print(html);
