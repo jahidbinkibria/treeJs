8499ad5dd48c0679b65e7f7b905bab36622602ff
Jahid Bin Kibria <jahidbinkibria@gmail.com>
2017-02-09 11:04:04 +0600

    random Number Function

--- pac.js
+++ pac.js
@@ -1,6 +1,5 @@
-// start with git
-var input = prompt("enter a number");
-var topNumber = parseInt(input);
-var randomNumber = Math.floor(Math.random() * topNumber)+1;
-var message = "<p>" + randomNumber + " is number between 1 and " + topNumber + "</p>"
-document.write(message);
+function randomfunction(randomnumber){
+	var upper = Math.floor(Math.random() * randomnumber)+1;
+	return upper;
+}
+document.write(randomfunction(20));
