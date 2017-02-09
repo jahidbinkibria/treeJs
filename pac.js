79cfb34d32f0f68fd48ddacd1be72eea73e44624
Jahid Bin Kibria <jahidbinkibria@gmail.com>
2017-02-09 11:42:14 +0600

    throw Error

--- pac.js
+++ pac.js
@@ -1,5 +1,15 @@
-function randomfunction(width,height,unit){
-	var area = width*height;
-	return (area + " " + unit);
+function randomfunction(lower,upper){
+	if(isNaN(lower) || isNaN(upper) ){
+		throw new Error('Both arguments must be numbers');
+	}
+	return Math.floor(Math.random()*(upper - lower +1))+lower;
 }
-document.write(6,8,' sq fit');	
+document.write(randomfunction(2,76));
+document.write("<br>");
+document.write(randomfunction(500,5687));
+document.write("<br>");
+document.write(randomfunction('hello',76));
+document.write("<br>");
+document.write(randomfunction(60,567));
+document.write("<br>");
+document.write(randomfunction(20,276));	
