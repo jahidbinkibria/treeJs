5798661d61be7f5b06881fa7d44c39eaa705836e
Jahid Bin Kibria <jahidbinkibria@gmail.com>
2017-02-09 11:57:57 +0600

    while loop

--- pac.js
+++ pac.js
@@ -1,15 +1,12 @@
-function randomfunction(lower,upper){
-	if(isNaN(lower) || isNaN(upper) ){
-		throw new Error('Both arguments must be numbers');
-	}
-	return Math.floor(Math.random()*(upper - lower +1))+lower;
+function randomfunction(upper){
+
+	return Math.floor(Math.random() * upper)+1;
+
 }
-document.write(randomfunction(2,76));
-document.write("<br>");
-document.write(randomfunction(500,5687));
-document.write("<br>");
-document.write(randomfunction('hello',76));
-document.write("<br>");
-document.write(randomfunction(60,567));
-document.write("<br>");
-document.write(randomfunction(20,276));	
+
+var count = 0;
+while(count<100){
+	var ramcount = randomfunction(100);
+	document.write(ramcount + ' ');
+	count++
+}
