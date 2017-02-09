92ad7da86aa5ff08befa9bff66b2ed86bcef097e
Jahid Bin Kibria <jahidbinkibria@gmail.com>
2017-02-10 03:39:26 +0600

    for loop

--- pac.js
+++ pac.js
@@ -1,19 +1,5 @@
-
-var randomNumber = getrandomnumber(10);
-var guess;
-var guessCount = 0; 
-var correctGuess = false;
-function getrandomnumber(upper){
-	var num =  Math.floor(Math.random() * upper)+1;
-	return num;
+var html = '';
+for(var i=1; i<=10; i++){
+	html += "<div>" + i + "</div>";
 }
-do{
-	guess = prompt('i am thinking of a number betwwen 1 and 10. what is it? ');
-	guessCount++
-	if(parseInt(guess) ===randomNumber){
-		correctGuess = true;
-	}
-}
-while(!correctGuess);
-document.write("<p> you guessed the number! </p>");
-document.write("its took you " + guessCount + " tires to guess the number " + randomNumber);
+document.write(html); 
