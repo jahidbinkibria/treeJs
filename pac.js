ea5065cfa483b8f691226d61d0a4e1623e3a703e
Jahid Bin Kibria <jahidbinkibria@gmail.com>
2017-02-10 10:04:20 +0600

    food shop array

--- pac.js
+++ pac.js
@@ -1,11 +1,33 @@
 var playList = [
-  'I Did It My Way',
-  'Respect',
-  'Imagine',
-  'Born to Run',
-  'Louie Louie',
-  'Maybellene'
+  'apple',
+  'orange',
+  'egg',
+  'watermallon',
+  'papaya',
+  'fish'
 ];
-
-playList.join(',');
-document.write(playList);
+var playList1 = [
+  'bread',
+  'biscute',
+  'milk',
+  'butter',
+  'pizza',
+  'cake'
+];
+var instock = playList.concat(playList1);
+var search;
+while(true){
+	search = prompt("search for a product in our store. type 'list' to show all type 'quit' to rest");
+	search = search.toLowerCase();
+	if(search === 'quit'){
+		break;
+	}else if(search === 'list'){
+		document.write(instock.join(', '));
+	}else{
+		if(instock.indexOf(search) > -1){
+			document.write('Yes! we have ' + search + ' in the store');
+		}else{
+			document.write('Sorry  we have not ' + search + ' in the store');
+		}
+	}
+}
