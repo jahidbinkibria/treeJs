2b0eb088e874959ba964ad26fc9c8de2037eae43
Jahid Bin Kibria <jahidbinkibria@gmail.com>
2017-02-12 03:22:05 +0600

    access object using loop

--- pac.js
+++ pac.js
@@ -1,20 +1,16 @@
-var playList = {
-	name: 'jahid',
-	id: 20,
-	batch: true,
-	subject: ['math','physics','c++']	
+var PlayList = {
+	Name: 'jahid',
+	Id: 20,
+	Batch: true,
+	Subject: ['math','physics','c++']	
 };
 
 function print(message){
 	var div = document.getElementById('hello');
 	div.innerHTML = message;
 }
-var message = '<p> hello my name is ' +  playList.name + '</p>'
-message += '<p> my id was ' +  playList.id + '</p>'
-playList.name = 'jahid khan';
-message += '<p> i wish my name was ' + playList.name + '</p>';
-playList.id += 1;
-message += '<p> my id is now ' + playList.id + '</p>';
-message+= '<p> i have subject ' + playList.subject.length + '</p>'
-message+= '<p> here the list : ' + playList.subject.join(', ');
-print(message);
+
+for(var i in PlayList){
+	console.log( i , ' :' ,PlayList[i]);
+}
+
