621e2c25105d7e9c1b10df8c1d2969dd5964dbe3
Jahid Bin Kibria <jahidbinkibria@gmail.com>
2017-02-12 02:11:47 +0600

    statrting with JS object

--- pac.js
+++ pac.js
@@ -1,21 +1,20 @@
-var playList = [
-  ['all of me','john legend'],
-  ['imagine','john lenon'],
-  ['love the way lie','eminem'],
-  ['dear mama','tupac'],
-  ['fix you','coldplay']
-];
+var playList = {
+	name: 'jahid',
+	id: 20,
+	batch: true,
+	subject: ['math','physics','c++']	
+};
 
 function print(message){
-	document.write(message);
+	var div = document.getElementById('hello');
+	div.innerHTML = message;
 }
-
-function printsongs(songs){
-	var listHTML = '<ol>';
-	for(var i = 0; i<songs.length; i++){
-		listHTML += '<li>' + songs[i][0] + ' by ' + songs[i][1] + '</li>'
-	}
-	listHTML += '</ol>';
-	print(listHTML);
-}
-printsongs(playList)
+var message = '<p> hello my name is ' +  playList.name + '</p>'
+message += '<p> my id was ' +  playList.id + '</p>'
+playList.name = 'jahid khan';
+message += '<p> i wish my name was ' + playList.name + '</p>';
+playList.id += 1;
+message += '<p> my id is now ' + playList.id + '</p>';
+message+= '<p> i have subject ' + playList.subject.length + '</p>'
+message+= '<p> here the list : ' + playList.subject.join(', ');
+print(message);
