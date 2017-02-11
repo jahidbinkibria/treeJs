3e4ef7d52c1fd51abf6a3c7832cc7f23616a4712
Jahid Bin Kibria <jahidbinkibria@gmail.com>
2017-02-12 04:02:15 +0600

    accessing arry object made a student list

--- pac.js
+++ pac.js
@@ -1,16 +1,55 @@
-var PlayList = {
-	Name: 'jahid',
-	Id: 20,
-	Batch: true,
-	Subject: ['math','physics','c++']	
-};
+var students = [
+	{ 
+		name: 'jahid',
+		track: 'javascript',
+		achivements: 175,
+		point: 12007
+	},
+	{ 
+		name: 'mehedi',
+		track: 'java',
+		achivements: 275,
+		point: 27007
+	},
+
+	{ 
+		name: 'robbani',
+		track: 'css',
+		achivements: 67,
+		point: 9007
+	},
+
+	{ 
+		name: 'shamim',
+		track: 'html',
+		achivements: 175,
+		point: 12067
+	},
+
+	{ 
+		name: 'nazmul',
+		track: 'python',
+		achivements: 15,
+		point: 807
+	},
+
+];
+
+var inHTML= '';
+var student;
 
 function print(message){
-	var div = document.getElementById('hello');
-	div.innerHTML = message;
+	var outputDiv = document.getElementById('hello');
+	outputDiv.innerHTML = message;
+} 
+
+for(var i=0; i<students.length;i++){
+	student = students[i];
+	inHTML += '<h2> Name - ' + student.name + '</h2>';
+	inHTML += '<p> Track : ' + student.track + '</p>';
+	inHTML += '<p> Achivements : ' + student.achivements + '</p>';
+	inHTML += '<p> Point : ' + student.point + '</p>';
 }
 
-for(var i in PlayList){
-	console.log( i , ' :' ,PlayList[i]);
-}
+print(inHTML);
 
